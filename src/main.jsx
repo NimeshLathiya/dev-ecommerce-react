90486118ae7921d50979412eda32ecd6ee7ffb04
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-11-30 08:10:26 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Auth0Provider } from "@auth0/auth0-react";
+import {  } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
